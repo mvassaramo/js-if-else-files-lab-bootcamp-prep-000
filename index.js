d41dce0ccc815c1e5967737108de301c9cc3cf63
maduri.vassaramo@hotmail.co.uk <maduri.vassaramo@hotmail.co.uk>
2018-04-23 18:01:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var height = "74"
 if (name === "Susan") {
   console.log("The name is correct")
 }
+else {
+  console.log("Expected the bame to be Susan")
+}
 
 // Don't worry about this
 module.exports = { name, height
